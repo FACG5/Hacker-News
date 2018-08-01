55289d8b55cb118bd73190841fc0482b69c1ebaa
donia98 <nadooshah98@gmail.com>
2018-08-01 15:00:10 +0300

    style and html & dom.js

--- src/server.js
+++ src/server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 
 const host = process.env.HOST || 'localhost';
-const port = process.env.PORT || 4000;
+const port = process.env.PORT || 3000;
 const router = require('./router');
 
 http.createServer(router).listen(port, () => {
